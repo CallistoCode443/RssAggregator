083eb8af1bd17381aeb8e2d803b1e49606d0bf27
Sovetov_YaV <sovetovyaroslav431@gmail.com>
2026-02-28 04:53:43 +0300

    initial commit

---
+++ src/test/java/com/example/rssaggregator/RssaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rssaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
